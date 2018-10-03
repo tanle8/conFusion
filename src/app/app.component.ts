2846ae187f7f1d732565fa10d283f99e41a6c2b0
Tan Le <tanld@vakaxa.com>
2018-10-03 10:55:39 -0700

    Component Part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html',  // Specify then name of the template file for this particular component (app)
+  styleUrls: ['./app.component.scss']   // Specify then name of the style file for this particular component (app)  
 })
 export class AppComponent {
   title = 'app';

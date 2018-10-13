2079809fabded273e1c2850d2050afd9465092e5
Tan Le <tanld@vakaxa.com>
2018-10-13 10:12:02 -0700

    Reactive Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 //  Modules
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatListModule } from "@angular/material/list";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { MatCardModule } from "@angular/material/card";
-import { MatButtonModule } from "@angular/material/button";
 import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatGridListModule } from "@angular/material/grid-list";
 import { MatInputModule } from "@angular/material/input";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { FormsModule } from "@angular/forms";
+import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 
 import 'hammerjs';
 
@@ -53,15 +56,18 @@ import { LeaderService } from "./services/leader.service";
     BrowserAnimationsModule,
     FlexLayoutModule,
     FormsModule,
-    MatToolbarModule,
-    MatListModule,
-    MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    MatCheckboxModule,
     MatDialogModule,
     MatFormFieldModule,
+    MatListModule,
+    MatToolbarModule,
+    MatGridListModule,
     MatInputModule,
-    MatCheckboxModule
+    MatSelectModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule
   ],
   // add service here
   providers: [
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Feedback, ContactType } from "../shared/feedback";
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  // Declare a variable named feedbackForm with typed FormGroup
+  feedbackForm: FormGroup;
+  // Declare a variable named feedback with typed Feedback we created before
+  feedback: Feedback;
+  contactType = ContactType;
+
+  constructor(private formbuilder: FormBuilder) {
+    this.createForm();
+
+   }
 
   ngOnInit() {
+  }
+
+  createForm() {
+    this.feedbackForm = this.formbuilder.group({
+      firstname: '',
+      lastname: '',
+      telnum: 0,
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    })
+  }
+
+  onSubmit() {
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset();
   }
 
 }
